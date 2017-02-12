62d0b5de05c063f4a4e1311ff034de7d1d7d5c3d
onocmouse <onocmouse@gmail.com>
2017-02-12 13:34:49 -0600

    Example of component stylesheet

--- app/components/Sample.js
+++ app/components/Sample.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import style from 'stylesheets/components/Sample.scss';
 
 export default ({sampleRequest}) => (
 	<button className='btn btn-primary' onClick={sampleRequest}>Click Me!</button>

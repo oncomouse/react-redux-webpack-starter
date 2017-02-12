2dacd56a7a42a44da6f82aca44e5058190e748d0
onocmouse <onocmouse@gmail.com>
2017-02-12 13:47:40 -0600

    REHYDRATE action example

--- app/reducers/samples.js
+++ app/reducers/samples.js
@@ -1,12 +1,17 @@
 import { Map } from 'immutable';
 import createReducer from 'utilities/createReducer';
 import { SAMPLE_ACTION } from 'constants/ActionTypes';
+import {REHYDRATE} from 'redux-persist/constants'
 
 const initialState = Map();
 const actionMaps = {
 	[SAMPLE_ACTION]: (state, action) => {
 		return state;
-	}
+	},
+	// Do something at REHYDRATE (when persisted store loads from storage)
+	//[REHYDRATE]: (state, action) => {
+	//	return state;
+	//}
 }
 
 export default createReducer(initialState, actionMaps);

60bd692f6782eac7ca695abd6779df74327f1102
oncomouse <oncomouse@gmail.com>
2017-08-07 18:57:02 -0500

    Removed error about using a Map as children

--- app/components/Sample.js
+++ app/components/Sample.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import styles from 'stylesheets/components/Sample.scss'
 
-export default ({samples, sampleRequest, resetRequest}) => (
+export default ({samples, sampleAction, resetAction}) => (
 	<div>
 		<ul>{
-			samples.map((sample,id) => (<li id={id}>{sample}</li>)).toSeq()
+			samples.valueSeq().map((sample,id) => (<li id={id} key={id}>{sample}</li>)).toSeq()
 		}</ul>
-		<button className={styles.test + ' btn btn-primary'} onClick={sampleRequest}>Click Me!</button>
-		<button className={styles.test + ' btn btn-primary'} onClick={resetRequest}>Reset List</button>
+		<button className={styles.test + ' btn btn-primary'} onClick={sampleAction}>Click Me!</button>
+		<button className={styles.test + ' btn btn-primary'} onClick={resetAction}>Reset List</button>
 	</div>
 )

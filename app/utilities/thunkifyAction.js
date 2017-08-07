77c1a7c795f92cb3035eb35166a10544a1b0eeb7
oncomouse <oncomouse@gmail.com>
2017-08-07 10:57:59 -0500

    Remove thunk + custom API middleware (use sagas)

--- app/utilities/thunkifyAction.js
+++
@@ -1,9 +0,0 @@
-// Wrap any action in thunk middleware so it can be chained w/ .then():
-export default function thunkifyAction(action) {
-	return (dispatch) => {
-		return new Promise((resolve) => {
-			dispatch(action);
-			resolve(action);
-		});
-	}
-}

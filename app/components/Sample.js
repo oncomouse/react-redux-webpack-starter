62b8b738e9d7c070baffdcae14f88c3d9c049143
oncomouse <oncomouse@gmail.com>
2017-08-07 19:02:57 -0500

    update

--- app/components/Sample.js
+++ app/components/Sample.js
@@ -4,7 +4,7 @@ import styles from 'stylesheets/components/Sample.scss'
 export default ({samples, sampleAction, resetAction}) => (
 	<div>
 		<ul>{
-			samples.valueSeq().map((sample,id) => (<li id={id} key={id}>{sample}</li>)).toSeq()
+			samples.valueSeq().map((sample,id) => (<li key={id}>{sample}</li>)).toSeq()
 		}</ul>
 		<button className={styles.test + ' btn btn-primary'} onClick={sampleAction}>Click Me!</button>
 		<button className={styles.test + ' btn btn-primary'} onClick={resetAction}>Reset List</button>

07d3942a9de9b7f8beb3536536e5644d69c34f6f
oncomouse <oncomouse@gmail.com>
2017-10-29 15:19:50 -0500

    Added styled-components support

--- app/components/Sample.js
+++ app/components/Sample.js
@@ -1,12 +1,24 @@
 import React from 'react'
-import styles from 'stylesheets/components/Sample.scss'
+// You can use CSS Modules if you prefer:
+//import styles from 'stylesheets/components/Sample.scss'
+import styled from 'styled-components'
+
+const Button = styled.button`
+	font-size: 1em;
+	margin: 1em;
+	padding: 0.25em 1em;
+	border-radius: 3px;
+	color: mediumseagreen;
+	background: white;
+	border: 2px solid mediumseagreen;
+`
 
 export default ({samples, sampleAction, resetAction}) => (
 	<div>
 		<ul>{
 			samples.valueSeq().map((sample,id) => (<li key={id}>{sample}</li>)).toArray()
 		}</ul>
-		<button className={styles.test + ' btn btn-primary'} onClick={sampleAction}>Click Me!</button>
-		<button className={styles.test + ' btn btn-primary'} onClick={resetAction}>Reset List</button>
+		<Button onClick={sampleAction}>Click Me!</Button>
+		<Button onClick={resetAction}>Reset List</Button>
 	</div>
 )

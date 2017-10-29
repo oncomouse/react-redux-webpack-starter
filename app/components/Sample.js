79fa874d1ab1a6b701b932e58b53d74954685dd0
oncomouse <oncomouse@gmail.com>
2017-10-29 15:19:50 -0500

    Added styled-components

--- app/components/Sample.js
+++ app/components/Sample.js
@@ -1,6 +1,18 @@
 import React from 'react'
 import R from 'ramda'
-import styles from 'stylesheets/components/Sample.scss'
+// You can use CSS Modules if you prefer:
+//import styles from 'stylesheets/components/Sample.scss'
+import styled from 'styled-components'
+
+const Button = styled.button`
+	font-size: 1em;
+	margin: 1em;
+	padding: 0.25em 1em;
+	border-radius: 3px;
+	color: mediumseagreen;
+	background: white;
+	border: 2px solid mediumseagreen;
+`
 
 export default ({samples, sampleAction, resetAction}) => (
 	<div>
@@ -10,7 +22,7 @@ export default ({samples, sampleAction, resetAction}) => (
 				R.mapObjIndexed((sample,id) => (<li key={id}>{sample}</li>))
 			)(samples)
 		}</ul>
-		<button className={styles.test + ' btn btn-primary'} onClick={sampleAction}>Click Me!</button>
-		<button className={styles.test + ' btn btn-primary'} onClick={resetAction}>Reset List</button>
+		<Button onClick={sampleAction}>Click Me!</Button>
+		<Button onClick={resetAction}>Reset List</Button>
 	</div>
 )

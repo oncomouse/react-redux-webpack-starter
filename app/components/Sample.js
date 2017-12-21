07ded71036411a52ac2247ecf1496e0d0e959cec
oncomouse <oncomouse@gmail.com>
2017-12-21 17:13:49 -0600

    Remove styled-components for tachyons

--- app/components/Sample.js
+++ app/components/Sample.js
@@ -2,18 +2,8 @@ import React from 'react'
 import R from 'ramda'
 // You can use CSS Modules if you prefer:
 //import styles from '../stylesheets/components/Sample.scss'
-import styled from 'styled-components'
 
-const Button = styled.button`
-	font-size: 1em;
-	margin: 1em;
-	padding: 0.25em 1em;
-	border-radius: 3px;
-	color: mediumseagreen;
-	background: white;
-	border: 2px solid mediumseagreen;
-`
-
+const BUTTON_STYLE = ' f6 link dim br3 ba ph3 pv2 mh1 mb2 dib  green b--green'
 export default ({ samples, sampleAction, resetAction }) => (
   <div>
     <ul>
@@ -26,11 +16,13 @@ export default ({ samples, sampleAction, resetAction }) => (
         )(samples)
       }
     </ul>
-    <Button onClick={sampleAction}>
-Click Me!
-    </Button>
-    <Button onClick={resetAction}>
-Reset List
-    </Button>
+    <button className={BUTTON_STYLE}
+      onClick={sampleAction}>
+      Click Me!
+    </button>
+    <button className={BUTTON_STYLE}
+      onClick={resetAction}>
+      Reset List
+    </button>
   </div>
 )

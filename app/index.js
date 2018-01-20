bd347e77cc7a830834312c4a08041ef0d0523fc8
oncomouse <oncomouse@gmail.com>
2018-01-20 15:34:34 -0600

    Add registerServiceWorker

--- app/index.js
+++ app/index.js
@@ -6,6 +6,7 @@ import { PersistGate } from 'redux-persist/lib/integration/react'
 import configStore from './store/configStore'
 import App from './containers/App'
 import loadPolyfills from './utilities/loadPolyfills'
+import registerServiceWorker from './utilities/registerServiceWorker'
 
 const { store, persistor } = configStore()
 
@@ -19,6 +20,7 @@ const render = Component => ReactDOM.render(
       </Provider>
     </PersistGate>
   </AppContainer>
-  , output)
-loadPolyfills().then(() => render(App))
+  , output
+)
+loadPolyfills().then(() => render(App)).then(() => registerServiceWorker())
 if (module.hot) module.hot.accept(['containers/App'], () => render(App))

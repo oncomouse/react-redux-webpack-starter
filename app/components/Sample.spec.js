c9c76544e43fc2f885952439aa114c919bd9c25c
oncomouse <oncomouse@gmail.com>
2018-01-25 16:46:13 -0600

    Working basic test framework

--- app/components/Sample.spec.js
+++ app/components/Sample.spec.js
@@ -1,9 +1,18 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
 import Sample from './Sample'
+import Adapter from 'enzyme-adapter-react-16'
+import { expect } from 'chai'
+import Enzyme from 'enzyme'
+import sinon from 'sinon'
 
-it('renders without crashing', () => {
-    const div = document.createElement('div')
-    ReactDOM.render(<Sample />, div)
-    ReactDOM.unmountComponentAtNode(div)
+Enzyme.configure({ adapter: new Adapter() })
+
+describe('<Sample/>', () => {
+	it('renders without crashing', () => {
+	    const wrapper = Enzyme.shallow(<Sample />)
+	})
+	it('should render 2 buttons', () => {
+		const wrapper = Enzyme.shallow(<Sample />)
+		expect(wrapper.find('button')).to.have.length(2);
+	})
 })

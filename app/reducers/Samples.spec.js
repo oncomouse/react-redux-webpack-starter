0af4eb9f92782181e151723d5c1647175feebe08
oncomouse <oncomouse@gmail.com>
2018-01-26 14:52:39 -0600

    Reducer testing

---
+++ app/reducers/Samples.spec.js
@@ -0,0 +1,24 @@
+import reducer from './Samples'
+import { sampleAction, resetAction } from '../actions/sampleActions'
+import { expect } from 'chai'
+
+describe('reducers/Samples', () => {
+    const initialState = {}
+    it('should return initialState', () => {
+        expect(reducer(undefined, {})).to.deep.equal(initialState)
+    })
+    it('should handle a sampleAction', () => {
+        const result = reducer(initialState, sampleAction())
+        expect(result).to.be.an('object')
+        expect(Object.keys(result).length).to.equal(1)
+        expect(Object.keys(result)[0]).to.be.a('string')
+        expect(result[Object.keys(result)[0]]).to.be.a('string')
+    })
+    it('should handle a resetAction', () => {
+        const result = reducer(
+            reducer(initialState, sampleAction())
+            , resetAction()
+        )
+        expect(result).to.deep.equal(initialState)
+    })
+})

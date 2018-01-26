732b4d2c462a94b6fcf32e6b7c4437a1164ade26
oncomouse <oncomouse@gmail.com>
2018-01-26 14:28:09 -0600

    Robust sample tests

--- app/components/Sample.spec.js
+++ app/components/Sample.spec.js
@@ -1,17 +1,18 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
 import Sample from './Sample'
 import { expect } from 'chai'
-//import sinon from 'sinon'
 import { shallow } from 'enzyme'
 
 const NUMBER_OF_BUTTONS = 2
 
 describe('<Sample/>', () => {
-    it('renders without crashing', () => {
-        const div = document.createElement('div')
-        ReactDOM.render(<Sample />, div)
-        ReactDOM.unmountComponentAtNode(div)
+    it('should render without crashing', () => {
+        const wrapper = shallow(<Sample />)
+        expect(wrapper.is('div')).to.equal(true)
+    })
+    it('should render a ul as the first child', () => {
+        const wrapper = shallow(<Sample />)
+        expect(wrapper.childAt(0).type()).to.equal('ul')
     })
     it(`should render ${NUMBER_OF_BUTTONS} buttons`, () => {
         const wrapper = shallow(<Sample />)
--- app/containers/App.spec.js
+++ app/containers/App.spec.js
@@ -1,21 +1,32 @@
 import React from 'react'
 import App from './App'
 import { expect } from 'chai'
-//import sinon from 'sinon'
-import { shallow, mount } from 'enzyme'
+import sinon from 'sinon'
+import { mount } from 'enzyme'
 import configureStore from 'redux-mock-store'
-
-const mockStore = configureStore()
-const NUMBER_OF_BUTTONS = 2
+import { sampleAction, resetAction } from '../actions/sampleActions'
 
 describe('<App/>', () => {
-    it('renders without crashing', () => {
-        const store = mockStore({})
-        const wrapper = shallow(<App store={store} />)// eslint-disable-line no-unused-vars
+    var store, wrapper, mockStore
+    before(() => {
+        mockStore = configureStore()
+        sinon.spy(App.prototype, 'componentDidMount')
     })
-    it(`should render ${NUMBER_OF_BUTTONS} buttons`, () => {
-        const store = mockStore({})
-        const wrapper = mount(<App store={store} />)
-        expect(wrapper.find('button')).to.have.length(NUMBER_OF_BUTTONS)
+    beforeEach(() => {
+        store = mockStore({})
+        wrapper = mount(<App store={store} />)
+    })
+    it('should render without crashing', () => {
+        expect(App.prototype.componentDidMount.calledOnce).to.equal(true)
+    })
+    it('should trigger a sampleAction when first button clicked', () => {
+        const expectedPayload = sampleAction()
+        wrapper.find('button').first().simulate('click')
+        expect(store.getActions()).to.deep.equal([expectedPayload])
+    })
+    it('should trigger a resetAction when last button clicked', () => {
+        const expectedPayload = resetAction()
+        wrapper.find('button').last().simulate('click')
+        expect(store.getActions()).to.deep.equal([expectedPayload])
     })
 })

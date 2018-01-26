567c62685e466c28f7dcb281f9d460d0f6d9c73f
oncomouse <oncomouse@gmail.com>
2018-01-26 14:34:56 -0600

    Better setup

--- app/components/Sample.spec.js
+++ app/components/Sample.spec.js
@@ -6,16 +6,17 @@ import { shallow } from 'enzyme'
 const NUMBER_OF_BUTTONS = 2
 
 describe('<Sample/>', () => {
+	var wrapper
+	beforeEach(() => {
+		wrapper = shallow(<Sample />)
+	})
     it('should render without crashing', () => {
-        const wrapper = shallow(<Sample />)
         expect(wrapper.is('div')).to.equal(true)
     })
     it('should render a ul as the first child', () => {
-        const wrapper = shallow(<Sample />)
         expect(wrapper.childAt(0).type()).to.equal('ul')
     })
     it(`should render ${NUMBER_OF_BUTTONS} buttons`, () => {
-        const wrapper = shallow(<Sample />)
         expect(wrapper.find('button')).to.have.length(NUMBER_OF_BUTTONS)
     })
 })

fae7be72db0d4c80e05381c74918050dd3ed7d58
oncomouse <oncomouse@gmail.com>
2018-01-30 16:09:02 -0600

    jsdom-global

--- test.prepare.js
+++ test.prepare.js
@@ -1,24 +1,6 @@
 const Enzyme = require('enzyme');
 const Adapter = require('enzyme-adapter-react-16');
-const {JSDOM} = require('jsdom');
+require('jsdom-global')()
 
 // Configure Enzyme for React 16:
 Enzyme.configure({ adapter: new Adapter() });
-
-// Configure JSDOM:
-function copyProps(src, target) {
-  const props = Object.getOwnPropertyNames(src)
-    .filter(prop => typeof target[prop] === 'undefined')
-    .reduce((result, prop) => ({
-      ...result,
-      [prop]: Object.getOwnPropertyDescriptor(src, prop),
-    }), {});
-  Object.defineProperties(target, props);
-}
-const dom = new JSDOM('<!doctype html><html><body></body></html>');
-global.window = dom.window;
-global.document = dom.window.document;
-global.navigator = {
-	userAgent: 'node.js'
-};
-copyProps(window, global);

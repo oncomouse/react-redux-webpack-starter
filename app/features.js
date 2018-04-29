e660bfb34e73a816e50888ced80fbd593ce54434
oncomouse <oncomouse@gmail.com>
2018-04-29 11:37:55 -0500

    Loading of sagas and store persistence can be dynamically configured.

---
+++ app/features.js
@@ -0,0 +1,4 @@
+// Does this project need to use redux-sagas?
+export const SAGAS = true;
+// Does this project need to use a persistent store?
+export const PERSIST = true;
--- app/index.jsx
+++ app/index.jsx
@@ -1,30 +1,34 @@
-import { AppContainer } from 'react-hot-loader';
-import ReactDOM from 'react-dom';
-import React from 'react';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/lib/integration/react';
-import configStore from './store/configStore';
-import App from './containers/App';
-import loadPolyfills from './utilities/loadPolyfills';
-import registerServiceWorker from './utilities/registerServiceWorker';
+import { AppContainer } from 'react-hot-loader'
+import ReactDOM from 'react-dom'
+import React from 'react'
+import { Provider } from 'react-redux'
+import { PERSIST } from './features'
+import configStore from './store/configStore'
+import App from './containers/App'
+import loadPolyfills from './utilities/loadPolyfills'
+import registerServiceWorker from './utilities/registerServiceWorker'
 
-const { store, persistor } = configStore();
+const { store, persistor } = configStore()
+
+const noopReactComponent = ({children}) => (<span>{children}</span>)
+
+const PersistGate = PERSIST ? require('redux-persist/lib/integration/react').PersistGate : noopReactComponent
 
 // React Hot Loading!
-const output = document.getElementById('react');
+const output = document.getElementById('react')
 const render = Component => ReactDOM.render(
-  <AppContainer>
-    <PersistGate persistor={persistor}>
-      <Provider store={store}>
-        <Component />
-      </Provider>
-    </PersistGate>
-  </AppContainer>
-  , output,
-);
-// loadPolyfills().then(() => render(App)).then(() => registerServiceWorker())
+    <AppContainer>
+        <PersistGate persistor={persistor}>
+            <Provider store={store}>
+                <Component />
+            </Provider>
+        </PersistGate>
+    </AppContainer>
+    , output
+)
+//loadPolyfills().then(() => render(App)).then(() => registerServiceWorker())
 loadPolyfills(() => {
-  render(App);
-  registerServiceWorker();
-});
-if (module.hot) module.hot.accept(['containers/App'], () => render(App));
+    render(App)
+    registerServiceWorker()
+})
+if (module.hot) module.hot.accept(['containers/App'], () => render(App))

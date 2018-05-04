50f580d2393d8eebd0e039f324dda0d6999821d1
oncomouse <oncomouse@gmail.com>
2018-05-04 15:14:42 -0500

    chai-as-promised support

--- test.prepare.js
+++ test.prepare.js
@@ -1,6 +1,10 @@
 const Enzyme = require('enzyme');
 const Adapter = require('enzyme-adapter-react-16');
+const chai = require("chai");
+const chaiAsPromised = require("chai-as-promised");
 require('jsdom-global')()
+
+chai.use(chaiAsPromised);
 
 // Configure Enzyme for React 16:
 Enzyme.configure({ adapter: new Adapter() });

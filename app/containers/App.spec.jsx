1d13ee1804621f347d5191b1dd47a568540db348
oncomouse <oncomouse@gmail.com>
2018-05-29 15:58:40 -0500

    So tired; but NYC says everything is covered.

--- app/containers/App.spec.jsx
+++ app/containers/App.spec.jsx
@@ -1,20 +1,18 @@
 import React from 'react';
 import { expect } from 'chai';
 import sinon from 'sinon';
-import fetchMock from 'fetch-mock';
 import { mount } from 'enzyme';
 import configureStore from 'redux-mock-store';
+import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import App from './App';
-import { sampleAction, resetAction } from '../ducks/Samples';
 
 describe('<App/>', () => {
-    let store
-        , wrapper
-        , mockStore;
+    let store;
+    let wrapper;
+    let mockStore;
     before(() => {
         mockStore = configureStore([thunk]);
-        fetchMock.get('*', { hello: 'world' });
     });
     beforeEach(() => {
         store = mockStore({
@@ -22,32 +20,13 @@ describe('<App/>', () => {
         });
         sinon.spy(App.prototype, 'componentDidMount');
         wrapper = mount(
-            <App
-                store={store}
-                actions={{ resetAction, sampleAction }}
-            />
+            <Provider store={store}><App /></Provider>
         );
-    });
-    after(() => {
-        fetchMock.reset();
-        fetchMock.restore();
     });
     afterEach(() => {
         App.prototype.componentDidMount.restore();
     })
     it('should render without crashing', () => {
         expect(App.prototype.componentDidMount).to.be.calledOnce;
-    });
-    it('should trigger a sampleAction when first button clicked', done => {
-        sampleAction()(store.dispatch, store.getState).then(expectedPayload => {
-            wrapper.find('button').first().simulate('click');
-            expect(store.getActions()).to.deep.equal([expectedPayload]);
-            done();
-        });
-    });
-    it('should trigger a resetAction when last button clicked', () => {
-        const expectedPayload = resetAction();
-        wrapper.find('button').last().simulate('click');
-        expect(store.getActions()).to.deep.equal([expectedPayload]);
     });
 });
